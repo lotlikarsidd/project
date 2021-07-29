8fe775d35ff8864fcbf9bc26a1d8b07fe38a813c
lotlikarsidd <72732640+lotlikarsidd@users.noreply.github.com>
2021-07-29 18:09:34 +0530

    Add files via upload

---
+++ Login/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Login, Owner, Customer
+
+admin.site.register(Login)
+admin.site.register(Owner)
+admin.site.register(Customer)
---
+++ Rent/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Rents, Tourist, Hotel
+
+admin.site.register(Rents)
+admin.site.register(Tourist)
+admin.site.register(Hotel)
